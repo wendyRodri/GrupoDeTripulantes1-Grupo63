e90a149ed78ea29512582b35714403bbcbcce449
jncvillal <jncvillal@gmail.com>
2021-10-08 20:35:33 -0400

    Front End y Back End de Incidentes

---
+++ cli/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="SISGO" src="../assets/SISO.jpeg">
+    <HelloWorld msg="Sistema Industrial de Salud y Seguridad Ocupacional"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
